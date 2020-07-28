30285d2da2e72873a068034d4ed49f3768bc116f
pranjanpr <pratyush.ranjan911@gmail.com>
2020-07-28 22:38:34 +0530

    more advanced

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -11,8 +12,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
-            messages.success(request,f'Account created for {username}!')
+            messages.success(request,f'Your Account has been Created!You are now able to Login.')
             return redirect('blog-home')
     else:
         form=UserRegisterForm()
     return render(request,'users/register.html',{'form': form})
+
+@login_required
+def profile(request):
+
+    return render(request,'users/profile.html')
+ 
